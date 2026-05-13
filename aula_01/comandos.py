ccf2f201fe07b50ea6a34cad7a5610d38977c48a
CatherinePimentel <pimentelcatherine01@gmail.com>
2026-05-12 21:23:34 -0300

    Inicia repositório aula 1 módulo 2

---
+++ aula_01/comandos.py
@@ -0,0 +1,11 @@
+
+# python -m venv meu_ambiente
+# source ./venv/Scripts/activate
+# Instalar bibliotecas :
+# pip intall pandas...
+# deactivate
+# rm -rf venv
+
+# Usar o arquivo requirements para fazer instalação de bibliotecas 
+# python -m venv venv
+# pip install -r requirements.txt
